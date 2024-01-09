2fbca5a61226fa48e63c709361574b411b539063
R894 <kamil.khalaileh@gmail.com>
2024-01-09 18:19:04 +0200

    Add Chatter project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "../components/ProjectCard";
 import Section from "../features/Section";
 import Portfolio from '../assets/images/portfolio.png'
 import Nuntium from "../assets/images/nuntium.png"
+import Chatter from "../assets/images/Chatter.png"
 
 const Projects = () => {
   return (
@@ -24,7 +25,17 @@ const Projects = () => {
           gitLink="https://github.com/R894/portfolio"
           demoLink="https://www.kamilkh.com/"
         >
-          Very long and interesting description
+          This is the website you're browsing right now!
+        </ProjectCard>
+
+        <ProjectCard
+          title="Chatter"
+          imgSrc={Chatter}
+          tags={["React", "Typescript", "Tailwindcss", "Docker", "Golang" ]}
+          gitLink="https://github.com/R894/chat-app"
+          demoLink="https://github.com/R894/chat-app"
+        >
+          Real-time chat Application, featuring a React frontend and a Golang backend.
         </ProjectCard>
       </div>
     </Section>
